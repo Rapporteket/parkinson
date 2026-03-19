c37ddba4503c4441de90188666e3892743fdb051
petterL <petter.linberg@helse-nord.no>
2026-03-19 13:43:37 +0100

    Conflict resolvement

--- R/server.R
+++ R/server.R
@@ -21,14 +21,13 @@ app_server <- function(input, output, session) {
     caller = "parkinson"
   )
 
-  meslinger_data <- getFakeRegData()
   data <- parkGetRegData()
   RegData <- data$RegData
   info_server("info", user = user, data = RegData)
   samlerapport_server("samlerapport")
   pivot_server("pivot", user = user)
   mod_fordeling_plot_server("fordeling", data = RegData)
-  mod_over_tid_server("over_tid", data = meslinger_data)
+  mod_over_tid_server("over_tid", data = RegData)
   mod_andeler_server("andeler", inputData = data)
 
   #################
